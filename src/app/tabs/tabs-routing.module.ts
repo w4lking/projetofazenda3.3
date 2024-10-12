23bf4f1704e985b0067351ab1711a18a0f8bf466
Layon Walker <layon.pedro@estudante.ufla.br>
2024-10-12 11:15:10 -0300

    Editando guards de tabs

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -6,31 +6,36 @@ const routes: Routes = [
   {
     path: '',
     component: TabsPage,
-    canActivate: [RedirectGuardGuard], // Aplica o guard para esta rota
     children: [
       {
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: 'tab2',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: 'tab4',
         loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: 'tabGraficos',
         loadChildren: () => import('../tab-graficos/tab-graficos.module').then( m => m.TabGraficosPageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: 'tabFazendas',
         loadChildren: () => import('../tab-fazendas/tab-fazendas.module').then( m => m.TabFazendasPageModule),
+        canActivate: [RedirectGuardGuard], // Aplique o guard aqui
       },
       {
         path: '',
@@ -38,8 +43,9 @@ const routes: Routes = [
         pathMatch: 'full'
       }
     ]
-  },
+  }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
