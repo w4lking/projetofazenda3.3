41a610d1aa9d17912cd367f18b9f11ad77fdf130
Layon Walker <layon.pedro@estudante.ufla.br>
2024-10-21 09:18:28 -0300

    Filtros ao adicionar funcionario, nome, cpf, email e senha

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -39,13 +39,13 @@ export class Tab4Page implements OnInit, ViewWillEnter {
 
   readonly maskPredicate: MaskitoElementPredicate = async (el) => (el as HTMLIonInputElement).getInputElement();
 
- 
+
 
   funcionarios: any[] = [];
   fazendas: any[] = [];
   idUsuario = Number(sessionStorage.getItem('id'));
   nome: string = "";
-  cpf : string = "";
+  cpf: string = "";
   email: string = "";
   telefone: string = "";
   senha: string = "";
@@ -109,20 +109,20 @@ export class Tab4Page implements OnInit, ViewWillEnter {
     this.telefone = funcionario.telefone;
     this.salario = funcionario.salario.toString();
     this.idFazenda = funcionario.fazendas_idfazenda;
-    
+
     console.log('idFazenda ao editar:', this.idFazenda); // Verifique se o valor é correto aqui
-    
+
     this.setOpen(true);
   }
-  
+
 
   getNomeFazenda(idFazenda: number | null): string {
     const fazenda = this.fazendas.find((f: { idfazendas: number | null }) => f.idfazendas == idFazenda); // Usando "==" para comparar diferentes tipos
     return fazenda ? fazenda.nome : 'Fazenda não encontrada';
   }
-  
-  
-  
+
+
+
 
   async obterFazendas() {
     const loading = await this.loadingController.create({
@@ -133,14 +133,14 @@ export class Tab4Page implements OnInit, ViewWillEnter {
     this.provider.obterFazenda(this.idUsuario).then(async (data: any) => {
       await loading.dismiss();
       if (data.status === 'success' && data.fazendas.length > 0) { // Verifica o status e se há fazendas
-          this.fazendas = data.fazendas; // Atribui os dados retornados diretamente
+        this.fazendas = data.fazendas; // Atribui os dados retornados diretamente
       } else {
-          this.fazendas = [];
+        this.fazendas = [];
       }
     }).catch(async (error) => {
       // await loading.dismiss();
       // this.mensagem('Erro ao carregar fazendas', 'danger');
-  });
+    });
   }
 
   async obterfuncionarios() {
@@ -172,15 +172,15 @@ export class Tab4Page implements OnInit, ViewWillEnter {
     this.telefone = telefone;
     this.salario = salario;
     this.idFazenda = idfazendas;  // Certifique-se de que o nome está correto
-  
+
     this.setOpen(true);  // Abre o modal para edição
   }
 
-  
+
   async salvarFuncionario() {
     if (this.editando) {
       console.log('idFazenda ao salvar (edição):', this.idFazenda); // Adicione um log aqui
-  
+
       this.provider.editarFuncionarios(
         this.idfuncionario,
         this.nome,
@@ -204,52 +204,74 @@ export class Tab4Page implements OnInit, ViewWillEnter {
       });
     } else {
       console.log('idFazenda ao salvar (novo):', this.idFazenda); // Verifique o valor aqui ao adicionar novo
-  
+
       this.adicionarFuncionario();
     }
   }
-  
-  
 
 
-async adicionarFuncionario() {
-  console.log('Dados enviados:', this.nome, this.cpf, this.email, this.telefone, this.salario, this.senha, this.idFazenda, this.idUsuario);
-  console.log('Tipo do idFazenda:', typeof this.idFazenda);
-  console.log('Tipo do idUsuario:', typeof this.idUsuario);
-  if (this.nome === '' || this.cpf === '' || this.email === '' || this.telefone === '' || this.salario === '' || this.idFazenda === null) {
-      this.presentToast();
-  } else {
-      const loading = await this.loadingController.create({
-          message: 'Adicionando funcionário...',
-      });
-      await loading.present();
 
-      this.provider.addFuncionarios(
-        this.nome, 
-        this.cpf, 
-        this.email, 
-        this.telefone, 
-        this.salario, 
-        this.senha, 
-        this.idFazenda, 
-        Number(this.idUsuario)  // Aqui garantir que idUsuario é um número
-      ).then(async (res: any) => {
-            await loading.dismiss();
-            if (res.status === 'success') {
-                this.exibirAlerta('Funcionario adicionado com sucesso!', 'success');
-                this.limpar();
-                this.obterfuncionarios(); // Atualiza a lista
-            } else {
-                this.mensagem('Erro ao adicionar funcionario. Tente novamente!', 'danger');
-            }
-        }
-    ).catch(async (error) => {
-        await loading.dismiss();
-        console.error('Erro na requisição ao adicionar funcionario:', error);
-        this.mensagem('Erro ao conectar-se ao servidor. Tente novamente!', 'danger');
+
+  async adicionarFuncionario() {
+    console.log('Dados enviados:', this.nome, this.cpf, this.email, this.telefone, this.salario, this.senha, this.idFazenda, this.idUsuario);
+    if (!this.validarCPF(this.cpf)) {
+      this.exibirAlerta('CPF inválido. Preencha corretamente!', 'danger');
+      return;
+    }
+    if (!this.validarEmail(this.email)) {
+      this.exibirAlerta('E-mail inválido. Preencha corretamente!', 'danger');
+      return;
+    }
+
+    if (this.senha.length < 8 || this.senha.length > 8) {
+      await this.exibirAlerta('A senha deve ter 8 caracteres', 'danger');
+      return;
+    }
+
+    // Verifica se a senha contém números sequenciais simples
+    if (this.contemNumerosSequenciais(this.senha)) {
+      await this.exibirAlerta(`A senha não deve conter números sequenciais simples como ${this.senha}.`, 'danger');
+      return;
+    }
+
+    // Verifica se a senha contém caracteres repetidos
+    if (this.contemCaracteresRepetidos(this.senha)) {
+      await this.exibirAlerta(`A senha não deve conter todos os caracteres iguais, como ${this.senha}.`, 'danger');
+      return;
+    }
+    
+
+    const loading = await this.loadingController.create({
+      message: 'Adicionando funcionário...',
     });
+    await loading.present();
+
+    this.provider.addFuncionarios(
+      this.nome,
+      this.cpf,
+      this.email,
+      this.telefone,
+      this.salario,
+      this.senha,
+      this.idFazenda,
+      Number(this.idUsuario)  // Aqui garantir que idUsuario é um número
+    ).then(async (res: any) => {
+      await loading.dismiss();
+      if (res.status === 'success') {
+        this.exibirAlerta('Funcionario adicionado com sucesso!', 'success');
+        this.limpar();
+        this.obterfuncionarios(); // Atualiza a lista
+      } else {
+        this.mensagem('Erro ao adicionar funcionario. Tente novamente!', 'danger');
+      }
+    }
+    ).catch(async (error) => {
+      await loading.dismiss();
+      this.mensagem('Erro ao conectar-se ao servidor. Tente novamente!', 'danger');
+    });
+
+    this.setOpen(false);
   }
-}
 
 
 
@@ -271,7 +293,7 @@ async adicionarFuncionario() {
     this.salario = maskitoTransform('R$ 0,00', salarioMask);
     this.idfuncionario;
     this.idFazenda;
-}
+  }
 
 
 
@@ -288,19 +310,19 @@ async adicionarFuncionario() {
     const alert = await this.alertController.create({
       message: mensagem,
     });
-  
+
     await alert.present();
-  
+
     // Fechar o alerta automaticamente após 3 segundos (3000 ms)
     setTimeout(() => {
       alert.dismiss();
     }, 500);
   }
-  
+
 
   // Função de confirmação para excluir o usuário
   async confirmarExclusaofuncionario(id: number) {
-  
+
     const alert = await this.alertController.create({
       header: 'Confirmação de exclusão',
       message: 'Tem certeza que deseja excluir este Funcionário?',
@@ -320,10 +342,10 @@ async adicionarFuncionario() {
         }
       ]
     });
-  
+
     await alert.present();
   }
-  
+
 
   // Função de exclusão
   excluirFuncionario(id: number) {
@@ -345,7 +367,56 @@ async adicionarFuncionario() {
       this.mensagem('Erro ao conectar-se ao servidor. Tente novamente!', 'danger');
     });
   }
-  
+
+  validarCPF(cpf: string): boolean {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11 || /^(\d)\1+$/.test(cpf)) {
+      return false;
+    }
+
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+      soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    if (resto !== parseInt(cpf.charAt(9))) {
+      return false;
+    }
+
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+      resto = 0;
+    }
+    return resto === parseInt(cpf.charAt(10));
+  }
+
+  validarEmail(email: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+
+  contemNumerosSequenciais(senha: string): boolean {
+    const sequencial = '0123456789';
+    for (let i = 0; i <= senha.length - 5; i++) {
+      if (sequencial.includes(senha.substring(i, i + 5))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Método para verificar caracteres repetidos
+  contemCaracteresRepetidos(senha: string): boolean {
+    return /^(\d)\1+$/.test(senha);
+  }
+
 
   async mensagem(mensagem: any, cor: string) {
     const toast = await this.toastController.create({
